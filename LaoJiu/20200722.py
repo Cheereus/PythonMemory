8582a583c1894b4c1822ea4c30c267148a06cbae
CheeReus_11 <fanwei1995@hotmail.com>
2020-07-22 15:07:59 +0800

    update 0722

--- LaoJiu/20200722.py
+++ LaoJiu/20200722.py
@@ -2,11 +2,14 @@
 @Description: 
 @Author: 陈十一
 @Date: 2020-07-22 10:06:12
-@LastEditTime: 2020-07-22 10:18:40
+@LastEditTime: 2020-07-22 10:55:18
 @LastEditors: 陈十一
 '''
 n = 0
 sum = 0
+
+# 直接求出所有数的次方备用
+pows = [pow(i, 5) for i in range(10)]
 
 # 最大只能有六位数
 for i in range(10, 999999):
@@ -19,13 +22,13 @@ for i in range(10, 999999):
   while n != 0:
 
     # 取余求次方
-    sum = sum + pow(n % 10, 5)
+    sum = sum + pows[n % 10]
     # 取整进位
     n = n // 10
     # 和已经超出就不必再算了
     if sum > i:
       break
-    
+
   if sum == i:
     print(sum)
 
