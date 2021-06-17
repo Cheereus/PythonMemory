728ffc7261db2e0c62c3e3373381235980ca539b
Cheereus <fanwei1995@hotmail.com>
2021-06-17 21:03:21 +0800

    updates

--- Mao/CompareSamples.py
+++ Mao/CompareSamples.py
@@ -7,7 +7,11 @@ lines = f.readlines()
 f.close()
 data_asa = []
 for i in trange(len(lines)):
-    line = list(map(str, lines[i].split(',')))
+    line = lines[i]
+    if '\n' in line:
+        line = line.replace('\n', '')
+        # print('回车')
+    line = list(map(str, line.split(',')))
     data_asa.append(line)
 data_asa = np.array(data_asa)
 
@@ -16,7 +20,11 @@ lines = f.readlines()
 f.close()
 data_xinyun = []
 for i in trange(len(lines)):
-    line = list(map(str, lines[i].split(',')))
+    line = lines[i]
+    if '\n' in line:
+        line = line.replace('\n', '')
+        # print('回车')
+    line = list(map(str, line.split(',')))
     data_xinyun.append(line)
 data_xinyun = np.array(data_xinyun)
 print(data_xinyun.shape)
@@ -29,26 +37,24 @@ Reverse_dict = {
 }
 
 for i in range(1, 17):
-    print('Comparing', i)
+    # print('Comparing', i)
     output = open('SamePos/Sample_' + str(i) + '.txt', 'w', encoding='utf-8')
-    output.writelines(' '.join(['asa_id', 'xinyun_id', 'chr', 'pos', 'asa-genotype', 'xinyun-genotype', 'same']) + '\n')
+    output.writelines(' '.join(['asa_id', 'xinyun_id', 'chr', 'pos', 'same', 'asa-genotype', 'xinyun-genotype']) + '\n')
     total = 0
     count = 0
     time.sleep(0.5)
-    for j in trange(data_xinyun.shape[0]):
+    for j in range(data_xinyun.shape[0]):
         xinyun = data_xinyun[j]
         asa = data_asa[j]
         # print(xinyun[0:1], xinyun[1].split(':'))
-        result = [asa[3], xinyun[0], xinyun[1].split(':')[0], xinyun[1].split(':')[1]]
-        if i == 16:
-            xinyun[i + 1] = xinyun[i + 1][:-1]
-        if len(xinyun[i + 1]) == 1:
-            xinyun[i + 1] = xinyun[i + 1] + xinyun[i + 1]
+        result = [asa[3], xinyun[1], xinyun[0].split(':')[0], xinyun[0].split(':')[1]]
+        REF = xinyun[2]
+        ALT = xinyun[3].split('-')
+        if len(xinyun[i + 3]) == 1:
+            xinyun[i + 3] = xinyun[i + 3] + xinyun[i + 3]
         if len(asa[i + 3]) == 1:
             asa[i + 3] = asa[i + 3] + asa[i + 3]
-        result.append(xinyun[i + 1])
-        result.append(asa[i + 3])
-        if len(xinyun[i + 1] + asa[i + 3]) == 4 and '-' not in xinyun[i + 1] + asa[i + 3] and 'I' not in xinyun[i + 1] + asa[i + 3] and 'D' not in xinyun[i + 1] + asa[i + 3]:
+        if len(xinyun[i + 3] + asa[i + 3]) == 4 and '.' not in xinyun[i + 3] + asa[i + 3] and 'I' not in xinyun[i + 3] + asa[i + 3] and 'D' not in xinyun[i + 3] + asa[i + 3] and 'U' not in xinyun[i + 3] + asa[i + 3] and '-' not in xinyun[i + 3] + asa[i + 3]:
             total += 1
             # if asa[0] == 'MINUS' or asa[0] == 'BOT':
             #     tmp = ''
@@ -56,17 +62,30 @@ for i in range(1, 17):
             #         tmp += Reverse_dict[p]
             #     asa[i + 3] = tmp
 
-            if xinyun[i + 1] == asa[i + 3] or xinyun[i + 1] == asa[i + 3][::-1]:
+            if xinyun[i + 3] == asa[i + 3] or xinyun[i + 3] == asa[i + 3][::-1]:
                 count += 1
                 result.append('T')
             else:
-                result.append('F')
+                tmp = ''
+                for p in asa[i + 3]:
+                    # print(asa[i + 3])
+                    tmp += Reverse_dict[p]
+                if xinyun[i + 3] == tmp or xinyun[i + 3] == tmp[::-1]:
+                    asa[i + 3] = tmp
+                    count += 1
+                    result.append('T')
+                else:
+                    result.append('F')
+
+        result.append(xinyun[i + 3])
+        result.append(asa[i + 3])
         if len(result) == 7:
             output.writelines(' '.join(result) + '\n')
 
     output.close()
     time.sleep(0.5)
-    print('均检出', total, '/', data_xinyun.shape[0])
-    print('一致数', count)
-    print('一致率', count / total)
-    print('----------')
+    print(i, data_xinyun.shape[0], total, count, count / total)
+    # print('均检出', total, '/', data_xinyun.shape[0])
+    # print('一致数', count)
+    # print('一致率', count / total)
+    # print('----------')
