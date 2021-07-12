b95a509d3cc995742f41c4040b3ef7d5473023f3
Cheereus <fanwei1995@hotmail.com>
2021-07-12 17:22:52 +0800

    updates

--- Mao/FindSamePos.py
+++ Mao/FindSamePos.py
@@ -2,22 +2,53 @@
 from tqdm import trange
 import numpy as np
 
-f = open('HNXD_YY_767_correct.bim', encoding='utf-8')
+same_counts = 0
+HNXD_counts = 0
+CFJY_counts = 0
+
+f = open('HNXD_YY_767_qc.bim', encoding='utf-8')
 lines = f.readlines()
 f.close()
 HNXD = {}
+HNXD_list = []
 for i in range(len(lines)):
     line = list(map(str, lines[i].split()))
     CHR, POS, ID = line[0], line[3], line[1]
     HNXD[CHR + ':' + POS] = ID
+    HNXD_list.append([CHR, POS, ID])
 
-f = open('CFJY_YY_4789_correct.bim', encoding='utf-8')
+f = open('CFJY_YY_4789_qc.bim', encoding='utf-8')
 lines = f.readlines()
 f.close()
-output = open('same_pos.lst', 'w', encoding='utf-8')
+output_same = open('same_pos.lst', 'w', encoding='utf-8')
+output_CFJY = open('CFJY_pos.lst', 'w', encoding='utf-8')
+CFJY = {}
+
 for i in trange(len(lines)):
     line = list(map(str, lines[i].split()))
     CHR, POS, ID = line[0], line[3], line[1]
+    CFJY[CHR + ':' + POS] = ID
     if CHR + ':' + POS in HNXD:
-        output.writelines(' '.join([HNXD[CHR + ':' + POS], ID]) + '\n')
-output.close()
+        output_same.writelines(' '.join([HNXD[CHR + ':' + POS], ID]) + '\n')
+        same_counts += 1
+    else:
+        output_CFJY.writelines(' '.join([ID]) + '\n')
+        CFJY_counts += 1
+
+output_same.close()
+output_CFJY.close()
+
+output_HNXD = open('HNXD_pos.lst', 'w', encoding='utf-8')
+for i in trange(len(HNXD_list)):
+    CHR, POS, ID = HNXD_list[i]
+    if CHR + ':' + POS in CFJY:
+        pass
+    else:
+        output_HNXD.writelines(' '.join([ID]) + '\n')
+        HNXD_counts += 1
+
+output_HNXD.close()
+
+print('Same:', same_counts)
+print('CFJY:', CFJY_counts)
+print('HNXD:', HNXD_counts)
